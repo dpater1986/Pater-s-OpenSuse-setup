1ee51871613cfe1853fcd20cfc0b8205ff7e6bf3
dpater1986 <d.pater1986@gmail.com>
2021-12-10 17:50:52 +0100

    update

--- opensuse-setup.sh
+++ opensuse-setup.sh
@@ -8,13 +8,13 @@ sudo zypper --non-interactive install wget ## used to get necessary files from g
 
 # 2. wget the necessary files from github
 wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/main/opensuse.packages
-wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/main/bashrc.aliases
+wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/main/bachrc.aliases
 
 # 3. Setup repositories for installations
 ## 3.1 Atom's repositorie
 sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ntype=rpm-md\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/zypp/repos.d/atom.repo'
 ## 3.2 Spotify-easyRPM repositorie
-sudo zypper addrepo https://download.opensuse.org/repositories/openSUSE:Factory/standard/openSUSE:Factory.repo
+sudo zypper ar https://download.opensuse.org/tumbleweed/repo/oss openSUSE:Factory
 ## 3.3 Microsoft teams
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper ar https://packages.microsoft.com/yumrepos/ms-teams/ ms-teams
