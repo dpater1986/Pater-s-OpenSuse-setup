0bdb7d38cd69f5308734a9abee635651736ba1c7
dpater1986 <d.pater1986@gmail.com>
2022-01-11 19:58:55 +0100

    test

--- opensuse-setup.sh
+++ opensuse-setup.sh
@@ -1,47 +1,114 @@
 #!/usr/bin/bash
+#
+# Setup my opensuse how I like to use it!
+# opensuse-setup.sh
+#
+clear
+
+echo "                      ______                      _       ______         _                                    ";
+echo "                      |  _  \                    (_)      | ___ \       | |                                   ";
+echo "                      | | | |  ___  _ __   _ __   _  ___  | |_/ /  __ _ | |_   ___  _ __                      ";
+echo "                      | | | | / _ \| '_ \ | '_ \ | |/ __| |  __/  / _\` || __| / _ \| '__|                     ";
+echo "                      | |/ / |  __/| | | || | | || |\__ \ | |    | (_| || |_ |  __/| |                        ";
+echo "                      |___/   \___||_| |_||_| |_||_||___/ \_|     \__,_| \__| \___||_|                        ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                                                                                              ";
+echo "                                         _____        _                                                       ";
+echo "                                        /  ___|      | |                                                      ";
+echo "                                        \ \`--.   ___ | |_  _   _  _ __                                        ";
+echo "                                         \`--. \ / _ \| __|| | | || '_ \                                       ";
+echo "                                        /\__/ /|  __/| |_ | |_| || |_) |                                      ";
+echo "                                        \____/  \___| \__| \__,_|| .__/                                       ";
+echo "                                                                 | |                                          ";
+echo "                                                                 |_|                                          ";
+echo " _____                                              _____         _      _                                  _ ";
+echo "|  _  |                                            |_   _|       | |    | |                                | |";
+echo "| | | | _ __    ___  _ __   ___  _   _  ___   ___    | |   _   _ | |__  | |  ___ __      __  ___   ___   __| |";
+echo "| | | || '_ \  / _ \| '_ \ / __|| | | |/ __| / _ \   | |  | | | || '_ \ | | / _ \\ \ /\ / / / _ \ / _ \ / _\` |";
+echo "\ \_/ /| |_) ||  __/| | | |\__ \| |_| |\__ \|  __/   | |  | |_| || |_) || ||  __/ \ V  V / |  __/|  __/| (_| |";
+echo " \___/ | .__/  \___||_| |_||___/ \__,_||___/ \___|   \_/   \__,_||_.__/ |_| \___|  \_/\_/   \___| \___| \__,_|";
+echo "       | |                                                                                                    ";
+echo "       |_|                                                                                                    ";
+
 cd ~
+update() {
+    echo "Update this script!"
+    wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/add_options/opensuse-setup.sh
+    chmod +x opensuse-setup.sh
+}
+version() {
+    echo "The script: ./opensuse-setup.sh" 
+    echo "is at verion 0.0.1"
+    echo "Do you like my script please donate at https://www.paypal.com/donate/?hosted_button_id=AKQYLCV9LU2S4"
+    echo "Thanks in advance"
+} 
+setup_ssh() {
+    echo 'test ssh'
+} 
 
-# 1. Update the system
-sudo zypper refresh
-sudo zypper update
-sudo zypper --non-interactive install wget ## used to get necessary files from github
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="Choose one of the following options:"
 
-# 2. wget the necessary files from github
-wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/main/opensuse.packages
-wget https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/main/bachrc.aliases
+OPTIONS=(u "Update this script"
+         V "Show version"
+         q "Quit")
 
-# 3. Setup repositories for installations
-## 3.1 Atom's repositorie
-sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ntype=rpm-md\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/zypp/repos.d/atom.repo'
-## 3.2 Spotify-easyRPM repositorie
-sudo zypper ar https://download.opensuse.org/tumbleweed/repo/oss openSUSE:Factory
-## 3.3 Microsoft teams
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo zypper ar https://packages.microsoft.com/yumrepos/ms-teams/ ms-teams
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
 
-## Update zypper to load gpg-keys and refresh
-sudo zypper --gpg-auto-import-keys refresh
-sudo zypper refresh
-## Check again for update
-sudo zypper update # I'll think you can't do this enough :-D
+clear
+case $CHOICE in
+        u)
+            echo "You chose to update this script"
+            clear
+            update
+            return;;
+        V)
+            echo "You chose to show the version of this script"
+            version
+            clear
+            return;;
+        q)
+            echo "You chose to quit"
+            clear
+            exit;;
+esac
 
-# 4. Installs all packages
-sudo zypper install $(cat ~/opensuse.packages)
-## install Spotify via spotify-easyrpm
-spotify-easyrpm
-
-# 5. Add personal aliases to .bachrc
-cat ~/bachrc.aliases >> ~/.bashrc
-
-# 6. Setup personal configuration files (wireguard, ssh keys and gnupg keys)
-## 6.1 Get files
-## 6.2 Import SSH keys
-### Local server
-rsync -avzhe ssh --progress pi@172.19.23.14:~/backup/ssh ~/importbackup
-cp ~/importbackup/ssh/home/dennis/.ssh/* ~/.ssh
-## 6.3 Import Gnupg keys
-## 6.4 Import Wireguard
-sudo mv ~/wg0.conf /etc/wireguard/wg0.conf
-sudo systemctl enable wg-quick@wg0.service
-sudo systemctl daemon-reload
-sudo systemctl start wg-quick@wg0.service
+while getopts ":m:s:u:V" option; do
+    case $option in
+        m) #Display menu
+        menu
+        exit;;
+        s) # Start ssh-setup
+        setup_ssh
+        exit;;
+        u) # Reinstall this script
+        update
+        exit;;
+        V) # Show version
+        version
+        exit;;
+        \?) # Invalid option
+        echo 'Load menu'
+        sleep 3
+        menu
+        exit;
+    esac
+done
