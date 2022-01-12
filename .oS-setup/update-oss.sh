fc46e910eb851616f18a288e06224c39c29d9472
dpater1986 <d.pater1986@gmail.com>
2022-01-12 12:55:43 +0100

    Update opensuse-setup.sh

--- .oS-setup/update-oss.sh
+++ .oS-setup/update-oss.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 wget -o ~/opensuse-setup.sh https://raw.githubusercontent.com/dpater1986/Pater-s-OpenSuse-setup/add_options/opensuse-setup.sh
 chmod +x ~/opensuse-setup.sh
 ./~/opensuse-setup.sh
